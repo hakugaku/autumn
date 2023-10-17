3659a2547716c4709f17ae6eb71e3d4730d37157
hakugaku <131091824+hakugaku@users.noreply.github.com>
2023-10-17 10:12:45 +0900

    Update ex6-5.js

--- ex6-5.js
+++ ex6-5.js
@@ -4,7 +4,7 @@ function sayhello65(){ alert('Hello,'+ who65 +'!');}
 
 var who1 ='someone';
 
-function coming1(){ alert(who1 + 'が来た');}
+function coming1(){ alert(who1 + 'が来た!');}
 
 
 function taro65(){ who65 = '太郎'; who1 = '太郎';}
