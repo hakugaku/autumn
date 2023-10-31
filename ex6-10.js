2a6fe77aed5e71f989d4ffb1653a5fa284bda402
hakugaku <131091824+hakugaku@users.noreply.github.com>
2023-10-31 10:30:04 +0900

    Update ex6-10.js

--- ex6-10.js
+++ ex6-10.js
@@ -1,2 +1,10 @@
 function imgchange1(){
-  document.getElementByld('logo').src='./image/bulbon.gif';}
+  document.getElementByld('logo').src=
+    './image/bulbon.gif';
+}
+
+
+function imgchange2() {
+document.getElementById('logo').src = 
+'./image/logo.png';
+}
