67e0c76b1f02b902e316aa19a7dc1252e4610a3f
hakugaku <131091824+hakugaku@users.noreply.github.com>
2023-10-31 09:43:29 +0900

    Create ex06-8.js

---
+++ ex06-8.js
@@ -0,0 +1,6 @@
+function sayhello68(){
+  alert('Hello,'+ document.getElementByld('who68').innerHTML+'!');}
+function taro68(){
+  document.getElementByld('who68').innerHTML='太郎';}
+function sayhello68(){
+  document.getElementByld('who68').innerHTML='花子';}
