5244c818030095fce33c455e7918dc4a24f09a66
hakugaku <131091824+hakugaku@users.noreply.github.com>
2023-11-07 10:45:16 +0900

    Create ex06-10-2.js

---
+++ ex06-10-2.js
@@ -0,0 +1,9 @@
+function imgchange3(){
+  document.getElementById('saku').src =
+    './image/sakura.png';
+}
+
+function imgchange4(){
+  document.getElementById('saku').src =
+    './image/sakura2.png';
+}
