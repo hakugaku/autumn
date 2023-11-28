b11e2a7193b33c5ba229662e8f683ddaf25e7f5c
hakugaku <131091824+hakugaku@users.noreply.github.com>
2023-11-28 10:12:20 +0900

    Update ex7-2.js

--- ex7-2.js
+++ ex7-2.js
@@ -1,5 +1,5 @@
 function add(){
-  var x = document.getElementByld('input1').value;
-  var y = document.getElementByld('input2').value;
+  var x = document.getElementById('input1').value;
+  var y = document.getElementById('input2').value;
   alert(x+y);
 }
