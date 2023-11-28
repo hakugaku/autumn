c12bb30ea3a02104e5ac948d664905c8a1980d88
hakugaku <131091824+hakugaku@users.noreply.github.com>
2023-11-28 10:28:45 +0900

    Create ex7-3.js

---
+++ ex7-3.js
@@ -0,0 +1,8 @@
+function bmi(){
+  var w = document.getElementByld('weight').value;
+  var y = document.getElementByld('height').value;
+  var z = y/100
+  var a = z*z
+  var b = w/a
+  alert(b);
+}
