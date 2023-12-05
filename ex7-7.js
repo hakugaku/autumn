286d5f018e452163f9c1d8705831485c7dcba155
hakugaku <131091824+hakugaku@users.noreply.github.com>
2023-12-05 10:48:56 +0900

    Update ex7-7.js

--- ex7-7.js
+++ ex7-7.js
@@ -1,7 +1,7 @@
 function nazo77(){
   if(document.getElementById('answer').value=='夢'||document.getElementById('answer').value=='ゆめ'){
     alert('あたり!');
-  }else {document.getElementById('answer').value!='夢'
+  }else {
     alert('はずれ!');
   }
 }
