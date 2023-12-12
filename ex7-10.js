381d4bd4090659717aec72b6953df67d7bd1b152
hakugaku <131091824+hakugaku@users.noreply.github.com>
2023-12-12 09:34:18 +0900

    Create ex7-10.js

---
+++ ex7-10.js
@@ -0,0 +1,16 @@
+  function bmiend(){
+  var w = document.getElementById('weight2').value;
+  var y = document.getElementById('height2').value;
+  var z = y/100
+  var a = z*z
+  var b = w/a
+  var final;
+    if (b < 18.5) {
+      final = '低体重';
+    } else if (b < 25) {
+      final = '普通';
+    } else {
+      final = '肥満';
+    }
+alert('あなたのBMIは ' + b + ' です。この数値は「' final '」の範囲に分類されます。');
+}
