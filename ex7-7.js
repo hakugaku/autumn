36ee20808ae511540638910638aa041f68f4f89a
hakugaku <131091824+hakugaku@users.noreply.github.com>
2023-12-12 10:09:13 +0900

    Update ex7-7.js

--- ex7-7.js
+++ ex7-7.js
@@ -1,7 +1,7 @@
 function nazo77(){
-  if (document.getElementById('answer773').value == '夢') {
+  if (document.getElementById('answer77').value == '夢') {
     alert('あたり!');
-  } else if (document.getElementById('answer773').value == 'ゆめ') {
+  } else if (document.getElementById('answer77').value == 'ゆめ') {
     alert('あたり!');
   } else {    
     alert('はずれ!');
