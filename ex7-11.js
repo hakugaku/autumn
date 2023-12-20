bac7fd608be2aa7b7b124f793babfa2615b10eaf
hakugaku <131091824+hakugaku@users.noreply.github.com>
2023-12-20 23:40:44 +0900

    Update ex7-11.js

--- ex7-11.js
+++ ex7-11.js
@@ -1,7 +1,7 @@
 function year() {
     var year1 = Number(document.getElementById('year711').value);
     var result;
-   if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+   if ((year1 % 4 == 0 && year1 % 100 != 0) || year1 % 400 == 0) {
         result = '閏年';
     } else {
         result = '平年';
